530c889c464b17f1ca261159e5b05c13edf29c61
Nicholas Martini <nicho.martini@gmail.com>
2020-02-19 17:32:01 +0100

    Add project files.

---
+++ StatBringers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatBringers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
